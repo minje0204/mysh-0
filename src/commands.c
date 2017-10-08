096a469ca496af92ee1210e2fe00642d06430670
minje0204 <minje@ajou.ac.kr>
2017-10-09 03:55:14 +0900

    submit

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,36 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
-    return -1;
+    return -1; 
+  chdir(argv[1]);  
 
-  // TODO: Fill it!
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
-    return -1;
-
-  // TODO: Fill it!
-
+ 	return -1;
+  char buff[255];
+  getcwd(buff,255);
+  printf("%s\n",buff);
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
+  if(argc == 1 || 
+     strcmp(argv[0], "cd") ||
+	 argc > 2)//(argc == 3 && !strcmp(argv[1], ".") && !strcmp(argv[1],".")))
+	 return 0;
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
-}
+   if(argc > 1 || strcmp(argv[0], "pwd"))
+   	return 0;
+   return 1;
+  }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,33 @@
-#include "utils.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char *real_command = command;
+	while(isspace(*(real_command++)));
+	real_command--;
+	char *end = strchr(real_command,'\n');
+	if(end)
+		*end = '\0';
+	*argc=0;
+	*argv=(char**)malloc(sizeof(*argv)*100);
+	char **temp=*argv;
+	if(*real_command != '\0')
+	{
+		*temp = strdup(strtok(strdup(real_command)," "));
+		while (*temp!=NULL)
+		{
+			*temp = strdup(*temp);
+			*(++temp)=strtok(NULL," ");
+			(*argc)++;
+		}
+	}
+	else
+	{
+		**argv = strdup("");
+		*argc = 1;
+	}
+	realloc(*argv,sizeof(*argv)*(*argc));
 }
